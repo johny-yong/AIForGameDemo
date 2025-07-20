398940ebf978ffe694cf54a2ed4f1701afe58b0c
ShiHeng0423 <42338127+ShiHeng0423@users.noreply.github.com>
2025-07-20 01:39:10 -0700

    Added Bullet Shooting

--- Assets/Assets/Scipts/PlayerController.cs
+++ Assets/Assets/Scipts/PlayerController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 //Basic controls
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;                 
+    public float moveSpeed = 5f;
+    public GameObject bullet;
     private Rigidbody2D rb;                      
     private Vector2 movement;                    
 
@@ -21,9 +24,18 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        movement = movement.normalized; 
+        movement = movement.normalized;
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0f;
 
+            Vector3 direction = (mouseWorldPos - transform.position).normalized;
+
+            GameObject obj = Instantiate(bullet, transform.position, Quaternion.identity);
+            obj.GetComponent<Bullet>().dir = direction;
+        }
     }
 
     //When using rigidbody, using FixedUpdate is better
---
+++ Assets/Bullet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [HideInInspector] public Vector3 dir = Vector3.zero;
+    public float speed = 5f;
+
+    void Update()
+    {
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+            Destroy(gameObject);
+    }
+}
+
